f549385c4596760b45eee9bbe9ad9e35fc151133
First-name Family-name <username@example.com>
2017-09-06 11:51:47 +0900

    third commit

--- PulseWaveSensor.ino
+++ PulseWaveSensor.ino
@@ -1,18 +1,43 @@
 #include <MsTimer2.h>
 
-#define SAMPLING_RATE 10  // Hz
+#define ANALOG_PIN    A0
+#define SAMPLING_RATE 100  // Hz
+#define START_BYTE 0xFE
+#define END_BYTE 0xFF
 
-void getAnalogValue() {
-  
+uint16_t data; // raw data
+uint8_t data_flag;
+
+void getPulseWave() {
+  data = analogRead(ANALOG_PIN);
+  data_flag = 1;
 }
 
 void setup() {
   // put your setup code here, to run once:
-  MsTimer2::set(500, intervalFunction);  // Set function called every set rate
-  MsTimer2::start();          // Starts timer interrupt
+  
+  // set up serial communication
+  Serial.begin(9600);
+  
+  // set up for sampling pulse wave
+  data_flag = 0;
+  MsTimer2::set(1000/ SAMPLING_RATE, getPulseWave);  // set function called every set interval ms
+  MsTimer2::start();          // start timer interrupt
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  if(data_flag) {
+    // clear flag
+    data_flag = 0;
+    
+    // transmit packets
+    // START_BYTE | upper data 3 bits | lower data 7 bits | END_BYTE
+    Serial.write(START_BYTE);
+    Serial.write((data >> 7) & 0b00000111);
+    Serial.write(data & 0b01111111);
+    Serial.write(END_BYTE);
+    
+    //Serial.println(data);
+  }
 }
